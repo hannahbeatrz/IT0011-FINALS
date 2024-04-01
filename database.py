4a220d212060893534e218280111053597ebaa00
hannahbeatrz <hannahbeatriz1995@gmail.com>
2024-04-01 16:13:19 +0800

    revised database.py

--- database.py
+++ database.py
@@ -1,49 +1,41 @@
 import sqlite3
 
 def create_table():
-    conn = sqlite3.connect('Products.db')
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS Products (id TEXT PRIMARY KEY, name TEXT, in_stock INTEGER)")
-    conn.commit()
-    conn.close()
-    
+    with sqlite3.connect('Products.db') as conn:
+        cur = conn.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS Products (
+                        id TEXT PRIMARY KEY,
+                        name TEXT,
+                        in_stock INTEGER
+                    )""")
+
 def fetch_products():
-    conn = sqlite3.connect('Products.db')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Products")
-    Products = cur.fetchall()
-    conn.close()
-    return Products
+    with sqlite3.connect('Products.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Products")
+        return cur.fetchall()
 
 def insert_product(id, name, in_stock):
-    conn = sqlite3.connect('Products.db')
-    cur = conn.cursor()
-    cur.execute("INSERT INTO Products (id, name, in_stock) VALUES (?,?,?)",(id,name,in_stock))
-    conn.commit()
-    conn.close()
-    
+    with sqlite3.connect('Products.db') as conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Products (id, name, in_stock) VALUES (?,?,?)", (id, name, in_stock))
+
 def delete_product(id):
-    conn = sqlite3.connect('Products.db')
-    cur = conn.cursor()
-    cur.execute("DELETE FROM Products WHERE id=?",(id,))
-    conn.commit()
-    conn.close()
-    
+    with sqlite3.connect('Products.db') as conn:
+        cur = conn.cursor()
+        cur.execute("DELETE FROM Products WHERE id=?", (id,))
+
 def update_product(new_name, new_stock, id):
-    conn = sqlite3.connect('Products.db')
-    cur = conn.cursor()
-    cur.execute("UPDATE Products SET name=?, in_stock=? WHERE id=?",(new_name, new_stock, id))
-    conn.commit()
-    conn.close()
-    
+    with sqlite3.connect('Products.db') as conn:
+        cur = conn.cursor()
+        cur.execute("UPDATE Products SET name=?, in_stock=? WHERE id=?", (new_name, new_stock, id))
+
 def id_exists(id):
-    conn = sqlite3.connect('Products.db')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Products WHERE id=?",(id,))
-    if cur.fetchone():
-        conn.close()
-        return True
-    conn.close()
+    with sqlite3.connect('Products.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Products WHERE id=?", (id,))
+        for row in cur:
+            return True
     return False
 
 create_table()
